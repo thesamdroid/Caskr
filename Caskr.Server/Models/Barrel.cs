ed3eb5b94d542cbf2e09561acc191aee1d63dffb
thesamdroid <39836126+thesamdroid@users.noreply.github.com>
2025-08-24 18:22:55 -0400

    Add barrel and rickhouse tracking

---
+++ Caskr.Server/Models/Barrel.cs
@@ -0,0 +1,24 @@
+namespace Caskr.server.Models;
+
+public partial class Barrel
+{
+    public int Id { get; set; }
+
+    public int CompanyId { get; set; }
+
+    public string Sku { get; set; } = null!;
+
+    public int BatchId { get; set; }
+
+    public int OrderId { get; set; }
+
+    public int RickhouseId { get; set; }
+
+    public virtual Company Company { get; set; } = null!;
+
+    public virtual Order Order { get; set; } = null!;
+
+    public virtual Rickhouse Rickhouse { get; set; } = null!;
+
+    public virtual Batch? Batch { get; set; }
+}
--- Caskr.Server/Models/Company.cs
+++ Caskr.Server/Models/Company.cs
@@ -14,5 +14,9 @@ public partial class Company
 
     public virtual ICollection<User> Users { get; set; } = new List<User>();
 
+    public virtual ICollection<Rickhouse> Rickhouses { get; set; } = new List<Rickhouse>();
+
+    public virtual ICollection<Barrel> Barrels { get; set; } = new List<Barrel>();
+
     public virtual User PrimaryContact { get; set; } = null!;
 }
