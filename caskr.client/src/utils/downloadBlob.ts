e322edfdd1c441ef37a3078115907d8e300976b8
thesamdroid <39836126+thesamdroid@users.noreply.github.com>
2025-09-22 15:18:28 -0400

    Fix document download and add coverage

---
+++ caskr.client/src/utils/downloadBlob.ts
@@ -0,0 +1,13 @@
+export const downloadBlob = (blob: Blob, filename: string) => {
+  const url = window.URL.createObjectURL(blob)
+  const anchor = document.createElement('a')
+  anchor.href = url
+  anchor.download = filename
+  anchor.style.display = 'none'
+  document.body.appendChild(anchor)
+  anchor.click()
+  document.body.removeChild(anchor)
+  window.setTimeout(() => {
+    window.URL.revokeObjectURL(url)
+  })
+}
